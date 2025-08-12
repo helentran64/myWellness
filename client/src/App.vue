f97b2bb6b117f52745f2c313f6f71fbb4ad1bc80
Helen Tran <helentran64@gmail.com>
2025-08-12 18:52:59 -0400

    added navbar

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,54 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import { useTheme } from 'vuetify'
+import { VBtn } from 'vuetify/components'
+
+const theme = useTheme()
+function toggleTheme() {
+  theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark'
+}
+</script>
 
 <template>
-  <h1>You did it!</h1>
+  <header>
+    <div>
+      <nav>
+        <div class="navbar">
+          <RouterLink to="/" class="router-link-item logo mt-2">
+            <i class="mdi mdi-food-apple"></i>
+            <span class="pl-1">myWellness</span>
+          </RouterLink>
+          <span class="navItems">
+            <v-btn @click="toggleTheme" icon="mdi-lightbulb-on" variant="plain" class="mb-2" />
+          </span>
+        </div>
+      </nav>
+    </div>
+  </header>
+  <RouterView />
 </template>
 
-<style scoped></style>
+<style scoped>
+.navbar {
+  padding: 15px;
+  display: flex;
+  justify-content: flex-start;
+  position: relative;
+  z-index: 1;
+}
+.router-link-item {
+  text-decoration: none;
+  border-radius: 4px;
+  margin-right: 10px;
+  color: inherit;
+  font-size: 1.3rem;
+}
+.navItems .router-link-item {
+  padding: 10px;
+}
+.navItems {
+  margin-left: auto;
+  margin-top: 5px;
+  font-weight: 600;
+}
+</style>
--- client/src/main.ts
+++ client/src/main.ts
@@ -1,12 +1,21 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import '@/assets/global.css'
 import App from './App.vue'
 import router from './router'
+import { createVuetify } from 'vuetify'
+import "vuetify/styles";
+import "@mdi/font/css/materialdesignicons.css";
+
+const vuetify = createVuetify({
+  theme: {
+    defaultTheme: 'dark',
+  },
+})
 
 const app = createApp(App)
-
 app.use(createPinia())
 app.use(router)
+app.use(vuetify)
 
 app.mount('#app')
